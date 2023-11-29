2be9e8ee33857892c692519b72964883964bbae4
Tomi Sakae <tomisakaeap1@gmail.com>
2023-11-29 17:12:16 +0700

    update

--- Program.cs
+++ Program.cs
@@ -10,6 +10,90 @@ namespace WaifuCardsBattle
     {
         static void Main(string[] args)
         {
+            menu();
+        }
+
+        static void menu()
+        {
+            int x = 5, y = 7;
+            int x_tam, y_tam;
+
+            while (true)
+            {
+                Console.SetCursorPosition(5, 5);
+                Console.WriteLine("Waifu Cards Battle");
+                Console.SetCursorPosition(7, 7);
+                Console.WriteLine("Chơi");
+                Console.SetCursorPosition(7, 9);
+                Console.WriteLine("Giới Thiệu");
+                Console.SetCursorPosition(7, 11);
+                Console.WriteLine("Thoát");
+
+
+                Console.SetCursorPosition(x, y);
+                Console.Write("->");
+
+
+                x_tam = x;
+                y_tam = y;
+
+                ConsoleKeyInfo lua_chon = Console.ReadKey();
+
+                switch (lua_chon.Key)
+                {
+                    case ConsoleKey.W:
+                        if (y >= 9)
+                            y -= 2;
+                        break;
+
+                    case ConsoleKey.S:
+                        if (y <= 9)
+                            y += 2;
+                        break;
+
+                    case ConsoleKey.K:
+                        Console.Clear();
+                        if (y == 7)
+                            Choi();
+                        if (y == 9)
+                            GioiThieu();
+                        if (y == 11)
+                            Thoat();
+                        Console.Clear();
+                        break;
+                }
+
+                Console.SetCursorPosition(x_tam, y_tam);
+                Console.Write("  ");
+            }
+        }
+
+        static void GioiThieu()
+        {
+            while (true)
+            {
+                Console.SetCursorPosition(5, 5);
+                Console.WriteLine("Đây là game chiến đấu với các thẻ bài Waifu!");
+                Console.SetCursorPosition(5, 5);
+
+                ConsoleKeyInfo lua_chon = Console.ReadKey();
+
+                switch (lua_chon.Key)
+                {
+                    case ConsoleKey.Q:
+                        return;
+                }
+            }
+
+        }
+
+        static void Thoat()
+        {
+            Environment.Exit(0);
+        }
+
+        static void Choi()
+        {
             
         }
     }
